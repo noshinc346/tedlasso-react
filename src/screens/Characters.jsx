6a5492c842e3d04334a157ec1192c6105a9f551d
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-11 16:43:19 -0400

    set getCharacters in Characters screen

--- src/screens/Characters.jsx
+++ src/screens/Characters.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+
+import { useState, useEffect } from 'react';
+import {getCharacters} from "../services/characters";
 
 function Characters() {
+  
+  const [characters, setCharacters] = useState([])
+
+  async function fetchCharacters() {
+    const allCharacters = await getCharacters()
+    setCharacters(allCharacters)
+  }
+
+  useEffect(() => {
+    fetchCharacters()
+  }, [])
+
+
   return (
     <div>Characters</div>
   )
